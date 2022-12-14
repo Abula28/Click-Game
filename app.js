66d624e61bb02c2560a470199e6c015ff6d47322
Abula <81083766+Abula28@users.noreply.github.com>
2022-12-14 23:50:34 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const main = document.querySelector('.main');
 const box = document.querySelector('.box')
 const score = document.querySelector('.score')
 let scoreCount = 0;
+let timer = document.querySelector('.timer')
+let timerCounter = 30;
+let timerInterval = 1000;
+let interval = setInterval(startTimer, timerInterval)
 
 const boxWidth = box.getBoundingClientRect().width
 const boxHeight = box.getBoundingClientRect().height
@@ -9,27 +13,31 @@ const boxHeight = box.getBoundingClientRect().height
 const mainWidth = main.getBoundingClientRect().width
 const mainHeight = main.getBoundingClientRect().height
 
+
 box.addEventListener('click', function(){
-    box.style.top = `${Math.floor(Math.random() * (mainHeight))}px`;
-    box.style.right = `${Math.floor(Math.random() * (mainWidth))}px`;
-    if(box.style.top == "0px"){
-        box.style.top = "100px"
-    }else if(box.style.top >= boxHeight){
-        box.style.top = `${boxHeight - 100}px`
+    let randomWidthHeight = Math.floor(Math.random() * 100)
+    box.style.top = `${Math.floor(Math.random() * (mainHeight - 100))}px`;
+    box.style.right = `${Math.floor(Math.random() * (mainWidth - 100))}px`;
+    box.style.width = `${randomWidthHeight}px`;
+    box.style.height = `${randomWidthHeight}px`;
+    if(randomWidthHeight < 30){
+        randomWidthHeight = 40;
+        box.style.width = `${randomWidthHeight}px`;
+        box.style.height = `${randomWidthHeight}px`;
     }
-    if(box.style.right == "0px"){
-        box.style.right == "100px"
-    }else if(box.style.right >= boxWidth){
-        box.style.left = "100px"
-    }
-    if(box.style.width == "10px"){
-        box.style.width = "20px"
-    }
-    if(box.style.height == "10px"){
-        box.style.height = "20px"
-    }
-    box.style.width = `${Math.floor(Math.random() * (boxWidth + 50))}px`;
-    box.style.height = `${Math.floor(Math.random()* (boxHeight + 50))}px`;
     scoreCount++;
     score.textContent = `Score ${scoreCount}`
 })
+
+function startTimer(){
+    timerCounter--;
+    timer.innerHTML = `Timer: ${timerCounter}`
+    if(timerCounter == 0){
+        endGame()
+    }
+}
+
+function endGame(){
+    Swal.fire(`Times out... Your Score Was: ${scoreCount}`)
+    clearInterval(interval)
+}
